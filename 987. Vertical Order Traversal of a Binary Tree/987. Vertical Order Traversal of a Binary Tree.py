22603449f19daeb4d9eca18db56e464394373e90
AsishramT <asishram.tamalampudi@gmail.com>
2026-06-22 22:43:06 -0400

    new solution added to the problem 987. Vertical Order Traversal of a Binary Tree

--- 987. Vertical Order Traversal of a Binary Tree/987. Vertical Order Traversal of a Binary Tree.py
+++ 987. Vertical Order Traversal of a Binary Tree/987. Vertical Order Traversal of a Binary Tree.py
@@ -23,4 +23,26 @@ class Solution:
             res[col].append(value)
 
         return [res[col] for col in res.keys()]
+
+#solution 2 (dfs)
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        nodes=[]
+        res=defaultdict(list)
+        def dfs(node,row,col):
+            if not node:
+                return
+            
+            nodes.append((col,row,node.val))
+            dfs(node.left,row+1,col-1)
+            dfs(node.right,row+1,col+1)
+
+        dfs(root,0,0)
+        nodes.sort()
+        
+        for col, row, value in nodes:
+            res[col].append(value)
+
+        return list(res.values())
+        
         
